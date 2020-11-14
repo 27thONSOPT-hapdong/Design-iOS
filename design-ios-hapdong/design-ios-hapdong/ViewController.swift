82070166b0f60ba3a16f93605b8a0405c44cb745
iamcho2 <thatgrass@naver.com>
2020-11-14 12:58:33 +0900

    add .gitignore

---
+++ design-ios-hapdong/design-ios-hapdong/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  design-ios-hapdong
+//
+//  Created by 초이 on 2020/11/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
